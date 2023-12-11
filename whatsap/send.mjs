40fd775a081d28a5c8fcab9239d4be8aabc458a7
nazarpunk <nazarpunk@gmail.com>
2023-12-11 17:34:22 +0200

    send message

--- whatsap/send.mjs
+++ whatsap/send.mjs
@@ -1,15 +1,45 @@
-import {Keys} from "./keys.mjs";
+import keys from './keys.mjs'
+
+/** @type {WhatsAppKeys} */
+const k = keys
+
+/** @typedef {Object} WhatsAppKeys
+ * @property {string} token
+ * @property {string} appId
+ * @property {string} phoneId
+ * @property {string} accountId
+ */
 
 /**
- * @param {string} message
- * @param {Keys} keys
+ * @param {string} phone
+ * @param {WhatsAppKeys} keys
  * @constructor
  */
-export const Send = (message, keys) => {
-    console.log(message, keys);
+export const Send = async (phone, keys) => {
+
+
+    const response = await fetch(`https://graph.facebook.com/v17.0/${keys.phoneId}/messages`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${keys.token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            messaging_product: 'whatsapp',
+            to: phone,
+            type: 'template',
+            template: {
+                name: 'hello_world',
+                language: {
+                    code: 'en_US'
+                }
+            }
+        })
+    })
+
+    const data = await response.json().catch(e => console.warn(e))
+    console.log('Data:', data)
 }
 
-const keys = Keys
 
-
-Send(`test ${Date.now()}`, keys);
+Send('380958828132', k)
